acdec6ed4b597c914d77e94eb6fc4f24d3cfbd37
ebcfireball <ebcftb@gmail.com>
2024-05-23 16:02:54 -0600

    home page mobile finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,174 @@
 import Image from "next/image";
+import Link from "next/link";
+import {useEffect, useState} from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gradient-to-r from-black to-slate-700 w-full">
+      <Navbar />
+      <FirstGlance />
+      <RealPropCards />
+      <WhyChoose />
+      <ContactUs />
+    </div>
+  );
+}
+
+// export function useIsVisible(ref) {
+//   const [isIntersecting, setIntersecting] = useState(false);
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(([entry]) => {
+//         setIntersecting(entry.isIntersecting)
+//     } 
+//     );
+    
+//     observer.observe(ref.current);
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, [ref]);
+
+//   return isIntersecting;
+// }
+
+function ContactUs(){
+  return (
+    <div className="w-4/5 mx-auto flex flex-col">
+      <button className="rounded-full font-semibold text-lg bg-cyan-700 text-gray-100 w-1/2 mx-auto my-8"><a href="">Contact Us</a></button>
+      <div className="flex flex-row">
+        <div className="basis-1/2">
+          <Image 
+            src="/Sky Logo PNG.png"
+            width={169}
+            height={65}
+            alt="sky realty co"
+          />
+        </div>
+        <div className="basis-1/2">
+          <Image
+            src="/passaroleasinglogo.png"
+            width={300}
+            height={83}
+            alt="passaro leasing"
+          />
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+function WhyChoose(){
+  return (
+    <div className="flex flex-col w-4/5 mx-auto h-fit mt-14">
+      <p className="font-bold text-lg text-center h-24 basis-1/8">Why Choose Us?</p>
+      <div className="basis-1/4 rounded-lg bg-slate-900 text-gray-300 p-4">
+        <p>
+          <Image
+            src="/quick.png"
+            width={70}
+            height={70}
+            alt=""
+          />
+        </p>
+        <p className="text-lg font-bold my-5">Quick Communication</p>
+        <p className="font-medium">We respond quickly to owners, tenants, and potentials.</p>
       </div>
+      <div className="basis-1/4 my-7 rounded-lg bg-slate-900 text-gray-300 p-4">
+        <p>
+          <Image
+              src="/productivity.png"
+              width={70}
+              height={70}
+              alt=""
+            />
+        </p>
+        <p className="text-lg font-bold my-5">Efficient Processes</p>
+        <p className="font-medium">Our technology allows property tasks to be done efficiently.</p>
+      </div>
+      <div className="basis-1/4 rounded-lg bg-slate-900 text-gray-300 p-4">
+        <p>
+          <Image
+              src="/knowledge.png"
+              width={70}
+              height={70}
+              alt=""
+            />
+        </p>
+        <p className="text-lg font-bold my-5">Expertise and Experience</p>
+        <p className="font-medium">We specialize in marketing, and getting properties rented quickly.</p>
+      </div>
+    </div>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function RealPropCards(){
+  return (
+    <div className="grid grid-cols-1 w-4/5 mx-auto">
+      <div className="flex flex-col w-full bg-slate-900 rounded-md p-6">
+        <p className="text-lg font-semibold">Real Estate</p>
+        <p className="font-medium mt-5">We help individuals find their perfect home, and investment properties.</p>
+        <button className="rounded-full bg-cyan-500 mt-4 text-gray-100 p-2 font-semibold "><a href="">Learn More</a></button>
+      </div>
+      <div className="flex flex-col w-full bg-slate-900 rounded-md my-10 p-6">
+        <p className="text-lg font-semibold">Property Management</p>
+        <p className="font-medium mt-5">We put high quality tenants in to keep money flowing into your pockets, stress free.</p>
+        <button className="rounded-full bg-cyan-500 mt-4 text-gray-100 p-2 font-semibold "><a href="">Learn More</a></button>
+      </div>
+    </div>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function FirstGlance(){
+  return (
+    <div className="grid grid-cols-1 w-4/5 mx-auto h-screen pt-14">
+      <Image 
+        src="/jebteamlogo.png"
+        width={340}
+        height={340}
+        alt="logo"
+        className="rounded-lg shadow-lg shadow-cyan-700 bg-slate-900 mx-auto"
+      />
+      <div className="mx-auto">
+        <p className="mb-5 text-xl font-bold">Jeb Real Estate and Property Management Team</p>
+        <p className="font-medium">We are here to take your real estate and property management game to the next level.</p> 
+        <p className="text-center font-bold mt-10">&darr;</p>
+      </div>
+    </div>
+  );
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+function Navbar(){
+  return (
+    <div className="navbar flex justify-center">
+      <div className="navbar-start flex flex-row justify-between w-4/5">
+        <div className="dropdown font-semibold text-lg">
+          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden ">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Contact</a></li>
+            <li><a href="">Team</a></li>
+            <li><a href="">Prop Mgmt</a></li>
+            <li><a href="">Real Estate</a></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl ">
+          <Image
+            src="/jebteamlogo.png"
+            width={48}
+            height={48}
+            alt=""
+          />
         </a>
       </div>
-    </main>
+      <div className="navbar-end hidden md:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Team</a></li>
+          <li><a>Contact</a></li>
+        </ul>
+      </div>
+    </div>
   );
 }
